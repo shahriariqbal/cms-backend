f6cafc78ec017d6ff272ba2e31779bacb5013038
Shahriar Iqbal <shahriar.iqbal.dev@gmail.com>
2023-12-02 20:17:48 +0200

    Updated Certificate model

--- models/certificate.js
+++ models/certificate.js
@@ -10,13 +10,80 @@ class Certificate {
   }
 
   // Create a new certificate
-  static create(name, data, callback) {}
+  static create(name, data, callback) {
+    db.run(
+      `CREATE TABLE IF NOT EXISTS certificates(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            data TEXT
+        )`,
+      [],
+      function (err) {
+        if (err) {
+          return callback(err);
+        }
+        db.run(
+          `INSERT INTO certificates(name, data) VALUES(?, ?)`,
+          [name, data],
+          function (err) {
+            if (err) {
+              return callback(err);
+            }
+            callback(null, new Certificate(this.lastID, name, data));
+          }
+        );
+      }
+    );
+  }
 
   // Get all certificates
-  static getAll(callback) {}
+  static getAll(callback) {
+    db.run(
+      `CREATE TABLE IF NOT EXISTS certificates(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            data TEXT
+        )`,
+      [],
+      function (err) {
+        if (err) {
+          return callback(err);
+        }
+        db.all(`SELECT * FROM certificates`, [], (err, rows) => {
+          if (err) {
+            return callback(err);
+          }
+          callback(
+            null,
+            rows.map((row) => new Certificate(row.id, row.name, row.data))
+          );
+        });
+      }
+    );
+  }
 
   // Delete a certificate
-  static delete(id, callback) {}
+  static delete(id, callback) {
+    db.run(
+      `CREATE TABLE IF NOT EXISTS certificates(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            data TEXT
+        )`,
+      [],
+      function (err) {
+        if (err) {
+          return callback(err);
+        }
+        db.run(`DELETE FROM certificates WHERE id = ?`, id, function (err) {
+          if (err) {
+            return callback(err);
+          }
+          callback(null, this.changes);
+        });
+      }
+    );
+  }
 }
 
 module.exports = Certificate;
