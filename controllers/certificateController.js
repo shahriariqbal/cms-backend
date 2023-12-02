81909e2cc6f59d857c7e67473ebfe40d12bcd981
Shahriar Iqbal <shahriar.iqbal.dev@gmail.com>
2023-12-02 20:21:41 +0200

    added crud operation in certificateController

--- controllers/certificateController.js
+++ controllers/certificateController.js
@@ -1,7 +1,38 @@
 const Certificate = require("../models/certificate");
 
-exports.createCertificate = (req, res) => {};
+exports.createCertificate = (req, res) => {
+  let name = req.body.name;
+  let data = req.body.data;
 
-exports.getAllCertificates = (req, res) => {};
+  Certificate.create(name, data, (err, certificate) => {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    res
+      .status(201)
+      .send(`A new certificate has been created with id ${certificate.id}`);
+  });
+};
 
-exports.deleteCertificate = (req, res) => {};
+exports.getAllCertificates = (req, res) => {
+  Certificate.getAll((err, certificates) => {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    res.send(certificates);
+  });
+};
+
+exports.deleteCertificate = (req, res) => {
+  let id = req.params.id;
+
+  Certificate.delete(id, (err, changes) => {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    if (changes === 0) {
+      return res.status(404).send("No certificate found with the given id");
+    }
+    res.send(`Certificate with id ${id} has been deleted`);
+  });
+};
